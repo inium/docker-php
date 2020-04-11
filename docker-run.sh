e4724d9f1adb8efeb7a6403b9c2c64a5757821d4
inlee <einable@gmail.com>
2020-04-11 18:52:54 +0900

    Update docker-run.sh
    - Delete unused text

--- docker-run.sh
+++ docker-run.sh
@@ -5,13 +5,13 @@ composer install
 
 # If the DOCKER_ENV env is production mode, run on production mode.
 if [ "${DOCKER_ENV}" == "production" ]; then
-    echo "***** Your app is production mode & . *****"
+    echo "***** Your app is production mode. *****"
 
     php-fpm
 
 # Otherwise, run on development mode.
 else
-    echo "***** Your app is development mode & . *****"
+    echo "***** Your app is development mode. *****"
 
     # Set Xdebug
     docker-php-ext-enable xdebug
@@ -26,18 +26,18 @@ else
     echo "xdebug.remote_autostart=1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
     echo "xdebug.remote_log=/usr/local/etc/php/xdebug.log" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 
-    echo "***** Xdebug is enabled *****"
+    echo "***** Xdebug is enabled. *****"
 
     # if the DOCKER_LARAVEL env is true, do Laravel execution.
     # run on /var/www/html/public directory files.
     if [ "${DOCKER_LARAVEL}" == true ]; then
-        echo "***** Laravel execution *****"
+        echo "***** Laravel execution. *****"
 
         php -S 0.0.0.0:80 -t public
 
     # Otherwise, run on /var/www/html directory files.
     else 
-        echo "***** General execution *****"
+        echo "***** General execution. *****"
 
         php -S 0.0.0.0:80
     fi
